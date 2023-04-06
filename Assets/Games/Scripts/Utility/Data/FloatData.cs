256d8abff70a62cc3eaf46b1de2d103a8f1331b7
Stefan Alfredsson <stefan@argentrealms.com>
2023-04-06 16:04:32 +0900

    Clarified FloatData class intentions.

--- Assets/Games/Scripts/Utility/Data/FloatData.cs
+++ Assets/Games/Scripts/Utility/Data/FloatData.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 namespace Games.Scripts.Utility.Data
 {
+    //The idea with this class is that in a bigger project data would be easier to re-use between many different objects.
+    //For example: instead of changing movement speeds of 10 different enemies in their prefabs, they could share a ScriptableObject and the designer just needs to change that single value.
+    //It could also be expanded to being settable at runtime, in which case it can be used to communicate data across objects with them needing a direct reference to each other.
     [CreateAssetMenu(menuName = "Shoot Em Up/Data/Float", fileName = "FloatData", order = 0)]
     public class FloatData : ScriptableObject
     {
